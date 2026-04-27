d79bde963a504946775346ced9ce47e45fcfcc78
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:48:19 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ https:/github.com/lightkirayagami/Leetcode/448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 27/04/2026, 21:48:18
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+    int n = nums.size();
+    vector<int>res;
+    unordered_map<int,int>temp;
+    for(int i = 1; i <= n; i++)  temp[i] = 0;
+    for(auto &i : nums)  temp[i]++;
+    for(auto &i : temp) {
+        if(i.second == 0) res.push_back(i.first);
+    }
+    return res;
+        
+    }
+};
