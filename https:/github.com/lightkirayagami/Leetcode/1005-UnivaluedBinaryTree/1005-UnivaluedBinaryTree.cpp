ac0a73862f3db3e3a404d5088c6c3673e9a8e8c7
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:46:32 +0530

    Create 1005-UnivaluedBinaryTree

---
+++ https:/github.com/lightkirayagami/Leetcode/1005-UnivaluedBinaryTree/1005-UnivaluedBinaryTree.cpp
@@ -0,0 +1,35 @@
+// Last updated: 27/04/2026, 21:46:32
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        
+        return solve(root,root->val);
+        
+    }
+    bool solve(TreeNode* root,int val)
+    {
+            if(root==NULL)
+            {
+                return true;
+            }
+        if(root->val!=val)
+        {
+            return false;
+        }
+        bool a=solve(root->left,val);
+        bool b=solve(root->right,val);
+        return a and b;
+    }
+    
+};
