e6e85fb05ffa801f08bdaadc4bc25633e86f84ae
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:53:18 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ https:/github.com/lightkirayagami/Leetcode/3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,47 @@
+// Last updated: 27/04/2026, 21:53:18
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int i=0;
+        int j=0;
+        unordered_map<char,int>mp;
+        int ans=0;
+        while(j<s.size())
+        {
+            mp[s[j]]++;
+
+            if(mp.size()<j-i+1)
+            {
+                while(mp.size()<j-i+1)
+                {
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0)
+                    {
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                
+            }
+            else if(mp.size()==j-i+1)
+            {
+                ans=max(ans,j-i+1);
+
+            }
+
+            j++;
+
+
+
+
+
+            
+            
+
+            
+        }
+        return ans;
+        
+    }
+};
