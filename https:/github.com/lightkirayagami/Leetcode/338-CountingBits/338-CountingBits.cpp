78b6f193a38cf2fe0fd4c95e4eb6d88142173cf1
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:48:57 +0530

    Create 338-CountingBits

---
+++ https:/github.com/lightkirayagami/Leetcode/338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,24 @@
+// Last updated: 27/04/2026, 21:48:56
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+
+        vector<int>dp(n+1);
+
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                dp[i]=dp[i/2];
+            }
+            else
+            {
+                dp[i]=dp[i/2]+1;
+            }
+        }
+
+        return dp;
+    }
+};
