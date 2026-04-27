4a69a53708042e7957f98ab05f2ca3598d56a824
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:45:50 +0530

    Create 1445-NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold

---
+++ https:/github.com/lightkirayagami/Leetcode/1445-NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold/1445-NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.cpp
@@ -0,0 +1,32 @@
+// Last updated: 27/04/2026, 21:45:49
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+
+        int i=0;
+        int j=0;
+        int n=arr.size();
+        int sum=0;
+        int count=0;
+        while(j<n)
+        {
+            sum=sum+arr[j];
+            if(j-i+1<k)
+            {
+                j++;
+            }
+            else if(j-i+1==k )
+            {
+                if((sum/k)>=threshold)
+                {
+                    count++;
+                }
+                sum=sum-arr[i];
+                i++;
+                j++;
+            }
+        }
+        return count;
+        
+    }
+};
