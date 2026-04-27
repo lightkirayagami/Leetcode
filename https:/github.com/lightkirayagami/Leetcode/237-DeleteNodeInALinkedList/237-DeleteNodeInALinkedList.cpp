18a32b7551e92f437174fd8490254162e2cd2d48
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:49:31 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ https:/github.com/lightkirayagami/Leetcode/237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.cpp
@@ -0,0 +1,17 @@
+// Last updated: 27/04/2026, 21:49:30
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+     node->val = node->next->val;
+node->next = node->next->next;
+        
+    }
+};
