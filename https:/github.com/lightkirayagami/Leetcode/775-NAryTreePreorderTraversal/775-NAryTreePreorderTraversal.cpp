603db286c151b97a5c97a7428dd19ab22df41033
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:47:10 +0530

    Create 775-NAryTreePreorderTraversal

---
+++ https:/github.com/lightkirayagami/Leetcode/775-NAryTreePreorderTraversal/775-NAryTreePreorderTraversal.cpp
@@ -0,0 +1,44 @@
+// Last updated: 27/04/2026, 21:47:09
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        
+        vector<int>ans;
+        preorder(ans,root);
+        return ans;
+    }
+    void preorder(vector<int>&ans,Node* root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        ans.push_back(root->val);
+         for(Node* const &it:root->children)
+         {
+             preorder(ans,it);  
+         }
+        return;
+             
+    }
+};
