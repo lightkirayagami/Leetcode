f90da65569c005ebc90795e51169e9f0c7e95b06
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:45:31 +0530

    Create 1955-SeatReservationManager

---
+++ https:/github.com/lightkirayagami/Leetcode/1955-SeatReservationManager/1955-SeatReservationManager.cpp
@@ -0,0 +1,35 @@
+// Last updated: 27/04/2026, 21:45:30
+class SeatManager {
+public:
+  priority_queue<int,vector<int>,greater<int>>minh;
+    SeatManager(int n) {
+
+      
+        for(int i=1;i<=n;i++)
+        {
+            minh.push(i);
+        }
+        
+    }
+    
+    int reserve() {
+        int ans=minh.top();  
+        minh.pop();
+        return ans;
+        
+    }
+    
+    void unreserve(int seatNumber) {
+        minh.push(seatNumber);
+
+
+        
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
