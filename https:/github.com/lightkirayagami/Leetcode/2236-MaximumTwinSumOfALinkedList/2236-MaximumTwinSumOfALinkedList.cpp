6f24b320eaeb6dfa92fd09f7d6d0f1b63cef0cc2
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:44:54 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ https:/github.com/lightkirayagami/Leetcode/2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.cpp
@@ -0,0 +1,70 @@
+// Last updated: 27/04/2026, 21:44:54
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        
+        
+        ListNode* head1=middle(head);
+        ListNode*head2= reverse(head1);
+
+        ListNode* curr1=head;
+        ListNode* curr2=head2;
+        int sum=0;
+        while(curr1&&curr2)
+        {
+            sum=max(curr1->val+curr2->val,sum);
+            curr1=curr1->next;
+            curr2=curr2->next;
+          
+        }
+
+        return sum;
+
+
+
+        
+        
+    }
+
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* forw=head;
+
+        while(curr)
+        {
+            forw=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forw;
+            
+            
+        }
+        return prev;
+
+    }
+    ListNode* middle(ListNode* head)
+    {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast&&fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+        
+    
+};
