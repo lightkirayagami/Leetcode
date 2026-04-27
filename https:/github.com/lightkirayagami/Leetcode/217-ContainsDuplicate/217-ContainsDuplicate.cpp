b0cd3c735b92f037a94b41aef7542f8520b6ec41
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:49:48 +0530

    Create 217-ContainsDuplicate

---
+++ https:/github.com/lightkirayagami/Leetcode/217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,20 @@
+// Last updated: 27/04/2026, 21:49:47
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(nums[i])!=mp.end())
+            {
+                return true;
+            }
+
+            mp[nums[i]]++;
+        }
+
+        return false;
+        
+    }
+};
