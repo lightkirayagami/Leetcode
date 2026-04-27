0db1285e41d53e8252841a44b810a65ba1719e80
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:49:25 +0530

    Create 257-BinaryTreePaths

---
+++ https:/github.com/lightkirayagami/Leetcode/257-BinaryTreePaths/257-BinaryTreePaths.cpp
@@ -0,0 +1,47 @@
+// Last updated: 27/04/2026, 21:49:24
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        string s="";
+        solve(root,ans,"" + to_string(root->val));
+        return ans;
+        
+    }
+    
+    void solve(TreeNode* root,vector<string>&ans,string s)
+    {
+        if(root->left==NULL&&root->right==NULL)
+        {
+           
+            
+            ans.push_back(s);
+            
+          return ;
+        }
+        
+        
+       if(root->left!=NULL)
+       {
+           solve(root->left,ans,s +"->" +to_string(root->left->val));
+       }
+         if(root->right!=NULL)
+       {
+           solve(root->right,ans,s +"->" +to_string(root->right->val));
+       }
+        return ;
+    }
+        
+    
+};
