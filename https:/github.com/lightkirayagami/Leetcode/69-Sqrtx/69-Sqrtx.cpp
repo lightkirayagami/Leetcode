c2d4de09531b76f1cf2f7d5684b400bf9b11d4cb
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:52:07 +0530

    Create 69-Sqrtx

---
+++ https:/github.com/lightkirayagami/Leetcode/69-Sqrtx/69-Sqrtx.cpp
@@ -0,0 +1,32 @@
+// Last updated: 27/04/2026, 21:52:07
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int start=0;
+        int end=x;
+        int ans=1;
+
+        while(start<=end)
+        {
+            long long int mid=start+(end-start)/2;
+            if(mid*mid==x)
+            {
+                return mid;
+            }
+            else if(mid*mid<x)
+            {   
+                ans=mid;
+                start=mid+1;
+            }
+            else
+            { 
+                end=mid-1;
+            }
+        }
+        return ans;
+
+
+        
+    }
+};
