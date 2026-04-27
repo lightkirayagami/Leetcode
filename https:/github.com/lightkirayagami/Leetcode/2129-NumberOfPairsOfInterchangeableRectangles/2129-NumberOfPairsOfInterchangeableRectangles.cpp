3e7ff20a78a9145aa40bef32f7f082c3d6aeb848
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:45:02 +0530

    Create 2129-NumberOfPairsOfInterchangeableRectangles

---
+++ https:/github.com/lightkirayagami/Leetcode/2129-NumberOfPairsOfInterchangeableRectangles/2129-NumberOfPairsOfInterchangeableRectangles.cpp
@@ -0,0 +1,18 @@
+// Last updated: 27/04/2026, 21:45:02
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        long long result = 0;
+	unordered_map<double, int> mp;
+
+	for (auto& rect : rectangles)
+	{
+		double ratio = (double)rect[0] / rect[1];
+		if(mp.find(ratio) != mp.end()) result += mp[ratio];
+		mp[ratio]++;
+	}
+
+	return result;
+        
+    }
+};
