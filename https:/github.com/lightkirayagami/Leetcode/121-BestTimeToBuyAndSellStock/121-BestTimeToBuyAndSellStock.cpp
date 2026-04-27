7e3a339cd324abeb5bf47acb21a9f427207b37ff
shatayu mittal <70124006+lightkirayagami@users.noreply.github.com>
2026-04-27 21:51:07 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ https:/github.com/lightkirayagami/Leetcode/121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,18 @@
+// Last updated: 27/04/2026, 21:51:06
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int minSoFar=INT_MAX;
+        int ans=0;
+
+
+        for(int i=0;i<prices.size();i++)
+        {
+            minSoFar=min(minSoFar,prices[i]);
+            ans=max(ans,prices[i]-minSoFar);
+        }
+        return ans;
+        
+    }
+};
